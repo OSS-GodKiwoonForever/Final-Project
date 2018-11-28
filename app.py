5a5aaf3f18456abd5a433c1e6ce1570941a77504
parksh <parksh5061@naver.com>
2018-11-28 20:38:21 +0900

    adsdf

--- app.py
+++ app.py
@@ -8,25 +8,7 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
-from urllib.request import Request, urlopen
-from urllib.parse import urlencode, quote_plus,unquote
-import xml.etree.ElementTree as ET
 
-API_key = unquote('공공데이터 포털에서 받은 API_KEY')
-url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst'
-queryParams = '?' + urlencode({ quote_plus('ServiceKey') : API_key, quote_plus('numOfRows') : '10', quote_plus('pageNo') : '1', quote_plus('itemCode') : 'PM10', quote_plus('dataGubun') : 'HOUR', quote_plus('searchCondition') : 'MONTH' })
-
-// Get 메소드를 통해 xml 가져오기
-request = Request(url + queryParams)
-request.get_method = lambda: 'GET'
-response_body = urlopen(request).read().decode('utf-8')
-root = ET.fromstring(response_body)
-
-// 특정 시,도 정보 파싱하기
-seoul = root.find('body').find('items').find('item').find('seoul')
-gyeonggi = root.find('body').find('items').find('item').find('gyeonggi')
-
-// 가져온 미세먼지 농도에 따라 결과 출력
 
 app = Flask(__name__)
 
@@ -50,6 +32,21 @@ def callback():
         abort(400)
     return 'OK'
 
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus,unquote
+import xml.etree.ElementTree as ET
+
+API_key = unquote('공공데이터 포털에서 받은 API_KEY')
+url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst'
+queryParams = '?' + urlencode({ quote_plus('ServiceKey') : API_key, quote_plus('numOfRows') : '10', quote_plus('pageNo') : '1', quote_plus('itemCode') : 'PM10', quote_plus('dataGubun') : 'HOUR', quote_plus('searchCondition') : 'MONTH' })
+
+request = Request(url + queryParams)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read().decode('utf-8')
+root = ET.fromstring(response_body)
+
+seoul = root.find('body').find('items').find('item').find('seoul')
+gyeonggi = root.find('body').find('items').find('item').find('gyeonggi')
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
