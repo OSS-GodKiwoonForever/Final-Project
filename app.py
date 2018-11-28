c65ee989036675e6dc4bf5193d1a67168919c3e6
parksh <parksh5061@naver.com>
2018-11-28 21:12:28 +0900

    adsdf

--- app.py
+++ app.py
@@ -8,6 +8,10 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
+from urllib.request import Request, urlopen
+from urllib.parse import urlencode, quote_plus,unquote
+import xml.etree.ElementTree as ET
+
 app = Flask(__name__)
 
 # Channel Access Token
@@ -33,8 +37,21 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+
+    API_key = unquote('R1V4MPrTQswXXkm8ChQgr%2BGl%2F%2F1SaMuMBpFpDZpflAftaVSnjVK%2F8ye6OZtNsdsyFbvfEsWfPdJAWX2soyzLeg%3D%3D')
+    url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst'
+    queryParams = '?' + urlencode({ quote_plus('ServiceKey') : API_key, quote_plus('numOfRows') : '10', quote_plus('pageNo') : '1', quote_plus('itemCode') : 'PM10', quote_plus('dataGubun') : 'HOUR', quote_plus('searchCondition') : 'MONTH' })
+
+    request = Request(url + queryParams)
+    request.get_method = lambda: 'GET'
+    response_body = urlopen(request).read().decode('utf-8')
+    root = ET.fromstring(response_body)
+
+    seoul = root.find('body').find('items').find('item').find('seoul')
+    gyeonggi = root.find('body').find('items').find('item').find('gyeonggi')
+
     # message = TextSendMessage(text=event.message.text)
-    message = TextSendMessage(text='나쁨')
+    message = TextSendMessage(text=seoul.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
