61c2caf36610b499980769d95b3671d94e6def84
parksh <parksh5061@naver.com>
2018-11-28 20:59:52 +0900

    adsdf

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     # message = TextSendMessage(text=event.message.text)
-    message = TextSendMessage(text='Hello world')
+    message = TextSendMessage(text=seoul.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
