6c9625a34abee12694b18189d3e7d877ca72b416
parksh5061 <42669772+parksh5061@users.noreply.github.com>
2018-11-30 18:34:35 +0900

    app.py
    측정 시간 출력 + PM10을 기준으로 현재 미세먼지 등급을 조회하고 출력해줌

--- app.py
+++ app.py
@@ -15,9 +15,9 @@ import xml.etree.ElementTree as ET
 app = Flask(__name__)
 
 # Channel Access Token
-line_bot_api = LineBotApi('Channel Access Token')
+line_bot_api = LineBotApi('gqg2I/lG+uFd3oNe/TBt7xVDYx8Um3PJufHx1ctTrutLi0PHzIU7UYMQ/w9eR5ZEn7qB0nmN9xJd0EuH+4VMiTOg+x29yZzrfxwqnLFMGNLVr5mPeYjyNTYirsa4028P4DPwB6SJdqADcDfrNJTq6gdB04t89/1O/w1cDnyilFU=')
 # Channel Secret
-handler = WebhookHandler('Channel Secret')
+handler = WebhookHandler('a6ff822dfad748a4f0e7582042a24634')
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -34,18 +34,30 @@ def callback():
         abort(400)
     return 'OK'
 # 處理訊息
+def air_status(data):
+    status = ""
+    if(0<int(data)<=30):
+        status = "좋음"
+    elif(30<int(data)<80):
+        status = "보통"
+    elif(80<int(data)<=150):
+        status = "나쁨"
+    elif(150<int(data)):
+        status = "매우나쁨"
+    return status
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
 
-    API_key = unquote('API_key')
+    API_key = unquote('R1V4MPrTQswXXkm8ChQgr%2BGl%2F%2F1SaMuMBpFpDZpflAftaVSnjVK%2F8ye6OZtNsdsyFbvfEsWfPdJAWX2soyzLeg%3D%3D')
     url = 'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureLIst'
-    queryParams = '?' + urlencode({ quote_plus('ServiceKey') : API_key, quote_plus('numOfRows') : '10', quote_plus('pageNo') : '1', quote_plus('itemCode') : 'PM10', quote_plus('dataGubun') : 'HOUR', quote_plus('searchCondition') : 'MONTH' })
+    queryParams = '?' + urlencode({ quote_plus('ServiceKey') : API_key, quote_plus('numOfRows') : '18', quote_plus('pageNo') : '1', quote_plus('itemCode') : 'PM10', quote_plus('dataGubun') : 'HOUR', quote_plus('searchCondition') : 'MONTH' })
 
     request = Request(url + queryParams)
     request.get_method = lambda: 'GET'
     response_body = urlopen(request).read().decode('utf-8')
     root = ET.fromstring(response_body)
 
+    date = root.find('body').find('items').find('item').find('dataTime')
     seoul = root.find('body').find('items').find('item').find('seoul')
     gyeonggi = root.find('body').find('items').find('item').find('gyeonggi')
     busan = root.find('body').find('items').find('item').find('busan')
@@ -63,9 +75,12 @@ def handle_message(event):
     gyeongnam = root.find('body').find('items').find('item').find('gyeongnam')
     jeju = root.find('body').find('items').find('item').find('jeju')
     sejong = root.find('body').find('items').find('item').find('sejong')
+
     dicts = {"서울":seoul,"경기":gyeonggi,"부산":busan,"대구":daegu,"인천":incheon,"광주":gwangju,"대전":daejeon,"울산":ulsan,"강원":gangwon,
     "충북":chungbuk,"충남":chungnam,"전북":jeonbuk,"전남":jeonnam,"경북":gyeongbuk,"경남":gyeongnam,"제주":jeju,"세종":sejong}
+
     textmsg = ""
+    result = ""
     #textmsg 변수 생성
     help_msg ="""입력이 잘못되었습니다.
 현재 입력 : {}
@@ -82,6 +97,7 @@ ex) "전체" , reply "서울의 미세먼지 수치: 94 울산의 미세먼지 �
 제주, 세종
 """
     userinput = event.message.text
+    textmsg = "측정 시간 : " + date.text + "\n"
     if (userinput == "전체"):
         for i in dicts.keys():
             textmsg += i+"의 PM10 수치:"+dicts[i].text+"\n" #for문을 돌면서 textmsg에 시도별 정보를 추가해줌
@@ -89,7 +105,10 @@ ex) "전체" , reply "서울의 미세먼지 수치: 94 울산의 미세먼지 �
     elif(userinput not in dicts.keys()):
         textmsg = help_msg.format(userinput)
     else:
-        textmsg = userinput.capitalize()+"의 PM10 수치:"+dicts[userinput].text    
+        textmsg += userinput.capitalize()+"의 PM10 수치:"+dicts[userinput].text + "\n"
+        result = air_status(dicts[userinput].text)
+        textmsg += "현재 미세먼지 농도 등급은 " + result + "입니다."
+
     message = TextSendMessage(text=textmsg)
     line_bot_api.reply_message(event.reply_token, message)
 
