c7dc71b2737e398f095a9a0c94c182e8f402b3fc
ChoDaeBeom <tmdgns743@naver.com>
2018-12-05 19:04:57 +0900

    add code

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ ex) "전체" , reply "서울의 미세먼지 수치: 94 울산의 미세먼지 �
         textmsg += "미세먼지:{:>3}".format(dicts[0][userinput].text) +", 초미세먼지:{:>3}".format(dicts[1][userinput].text)+ "\n"
         result = air_status(dicts[0][userinput].text)
         textmsg += "현재 미세먼지 농도 등급은 " + result + "입니다."
-		if(result == "보통" or result == "좋음"):
+        if(result == "보통" or result == "좋음"):
     		if(userinput=="전남" or userinput=="전북"or userinput=="광주"): #전라도 방언 조건문
     			textmsg += "\n" + "오늘은 아따 거시기 하기에 양호하다잉."
     			elif(userinput=="부산" or userinput=="대구"or userinput=="울산"or userinput=="경북"or userinput=="경남"): #경상도 방언 조건문
